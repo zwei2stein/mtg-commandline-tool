08b3096833946ed9fca0f7daff0ad6b92fb272cd
Petr Prokop <petr_prokop@kb.cz>
2018-10-22 09:38:23 +0200

    Cache timeout for scryfall cards

--- scryfall.py
+++ scryfall.py
@@ -13,6 +13,9 @@ auth = None
 
 valid_filename_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
+clearCache = 'none'
+cacheTimeout = 365
+
 def cleanFilename(filename, whitelist=valid_filename_chars, replace=' '):
 	# replace spaces
 	for r in replace:
@@ -24,6 +27,20 @@ def cleanFilename(filename, whitelist=valid_filename_chars, replace=' '):
 	# keep only whitelisted chars
 	return ''.join(c for c in cleaned_filename if c in whitelist)
 
+def fetchCardJson(cardName, jsonFile):
+	response = requests.get("http://api.scryfall.com/cards/named",  params={'exact': cardName}, proxies=proxies, auth=auth)
+	if (response.status_code == 404):
+		print ()
+		print (console.CRED + "Card '" + cardName + "' Was not found in scryfall using exact search." + console.CEND + " Trying fuzzy search.")
+		response = requests.get("http://api.scryfall.com/cards/named",  params={'fuzzy': cardName}, proxies=proxies, auth=auth)
+		if (response.status_code < 400):
+			print ("\'" + card.name + "\' found as \'" + response.json()["name"] + "\'.")
+	if (response.status_code >= 400):
+		raise Exception('Bad response ' + str(response.status_code) + ' for ' + cardName) 
+	with open(jsonFile, 'w') as f:
+		json.dump(response.json(), f)
+	return response.json()
+
 def getCachedCardJson(card):
 	baseDir = os.path.join(os.path.dirname(sys.argv[0]), ".scryfallCache")
 	jsonFile = os.path.join(baseDir, cleanFilename(card.name) + ".json")
@@ -31,23 +48,14 @@ def getCachedCardJson(card):
 		os.makedirs(baseDir)
 	if (os.path.exists(jsonFile)):
 		fileAge = datetime.date.today() - datetime.date.fromtimestamp(os.path.getmtime(jsonFile))
+
+		if (clearCache == 'always' or (clearCache == 'timeout' and fileAge > cacheTimeout) or (clearCache == 'price' and fileAge > 1)):
+			return fetchCardJson(card.Name, jsonFile)
+		else:
 #		print("Loading cached " + jsonFile)
-		with open(jsonFile, encoding='utf-8') as json_data:
-			return json.load(json_data)
+			with open(jsonFile, encoding='utf-8') as json_data:
+				return json.load(json_data)
 	else:
 #		print("Loading online " + jsonFile)
-		response = requests.get("http://api.scryfall.com/cards/named",  params={'exact': card.name}, proxies=proxies, auth=auth)
-		if (response.status_code == 404):
-			print ()
-			print (console.CRED + "Card '" + card.name + "' Was not found in scryfall using exact search." + console.CEND + " Trying fuzzy search.")
-			response = requests.get("http://api.scryfall.com/cards/named",  params={'fuzzy': card.name}, proxies=proxies, auth=auth)
-			if (response.status_code < 400):
-				print ("\'" + card.name + "\' found as \'" + response.json()["name"] + "\'")
-		
-		if (response.status_code >= 400):
-			raise Exception('Bad response ' + str(response.status_code) + ' for ' + card.name) 
-		with open(jsonFile, 'w') as f:
-			json.dump(response.json(), f)
-		return response.json()
-
+		return fetchCardJson(card.Name, jsonFile)
 #
