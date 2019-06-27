ffa41b83f10f72fdb8ee1ddade47fb5a11808923
Petr Prokop <petr_prokop@kb.cz>
2019-06-27 12:41:15 +0200

    Removed frontier from scryfall

--- nameDeck.py
+++ nameDeck.py
@@ -7,6 +7,8 @@ import util
 
 from decimal import *
 
+import random
+
 colorPairingNames = {
  'W': ['White', 'Monowhite', 'Mono-white'],
  'U': ['Blue', 'Monoblue', 'Mono-blue'],
@@ -41,7 +43,7 @@ colorPairingNames = {
  'WUBRG': ['Prismatic', '5-Color', 'Multicolor', 'Rainbow', 'Domain'],
  'C': ['Colorless', 'Monobrown', 'Diamond']}
 
-keywordList = ["Abandon", "Absorb", "Actions", "Activate", "Adapt", "Addendum", "Affinity", "Afflict", "Afterlife", "Aftermath", "Amplify", "Annihilator", "Ascend", "Assist", "Attach", "Aura Swap", "Awaken", "Banding", "Battalion", "Battle Cry", "Bestow", "Bloodrush", "Bloodthirst", "Bolster", "Bushido", "Buyback", "Cascade", "Cast", "Champion", "Changeling", "Channel", "Chroma", "Cipher", "Clash", "Cohort", "Conspire", "Constellation", "Converge", "Convoke", "Council's Dilemma", "Counter", "Crew", "Cumulative Upkeep", "Cycling", "Dash", "Deathtouch", "Defender", "Delirium", "Delve", "Destroy", "Detain", "Dethrone", "Devoid", "Devour", "Discard", "Domain", "Double Agenda", "Double Strike", "Double", "Dredge", "Echo", "Embalm", "Emerge", "Eminence", "Enchant", "Enrage", "Entwine", "Epic", "Equip", "Escalate", "Eternalize", "Evergreen", "Evoke", "Evolve", "Exalted", "Exchange", "Exert", "Exile", "Exploit", "Explore", "Extort", "Fabricate", "Fading", "Fateful Hour", "Fateseal", "Fear", "Ferocious", "Fight", "First Strike", "Flanking", "Flash", "Flashback", "Flying", "Forecast", "Formidable", "Fortify", "Frenzy", "Fuse", "Goad", "Graft", "Grandeur", "Gravestorm", "Haste", "Haunt", "Hellbent", "Heroic", "Hexproof", "Hidden Agenda", "Hideaway", "Horsemanship", "Imprint", "Improvise", "Indestructible", "Infect", "Ingest", "Inspired", "Intimidate", "Investigate", "Join Forces", "Jump-Start", "Keyword", "Kicker", "Kinship", "Landfall", "Landwalk", "Level Up", "Lieutenant", "Lifelink", "Living Weapon", "Madness", "Manifest", "Mechanics", "Megamorph", "Meld", "Melee", "Menace", "Mentor", "Metalcraft", "Miracle", "Modular", "Monstrosity", "Morbid", "Morph", "Multikicker", "Myriad", "Ninjutsu", "Offering", "Other", "Outlast", "Overload", "Parley", "Partner with", "Partner", "Persist", "Phasing", "Planeswalk", "Play", "Poisonous", "Populate", "Proliferate", "Protection", "Provoke", "Prowess", "Prowl", "Radiance", "Raid", "Rally", "Rampage", "Reach", "Rebound", "Recover", "Regenerate", "Reinforce", "Renown", "Replicate", "Retrace", "Reveal", "Revolt", "Riot", "Ripple", "Scavenge", "Scry", "Set in Motion", "Shadow", "Shroud", "Shuffle", "Skulk", "Soulbond", "Soulshift", "Spectacle", "Spell Mastery", "Splice", "Split Second", "Storm", "Strive", "Sunburst", "Support", "Surge", "Surveil", "Suspend", "Sweep", "Tempting Offer", "Threshold", "Totem Armor", "Trample", "Transfigure", "Transform", "Transmute", "Tribute", "Typecycling", "Undaunted", "Undergrowth", "Undying", "Unearth", "Unleash", "Untap", "Vanishing", "Vigilance", "Vote", "Will of the Council", "Wither", "Words"]
+keywordList = ["Abandon", "Absorb", "Actions", "Activate", "Adapt", "Addendum", "Affinity", "Afflict", "Afterlife", "Aftermath", "Amplify", "Annihilator", "Ascend", "Assist", "Attach", "Aura Swap", "Awaken", "Banding", "Battalion", "Battle Cry", "Bestow", "Bloodrush", "Bloodthirst", "Bolster", "Bushido", "Buyback", "Cascade", "Cast", "Champion", "Changeling", "Channel", "Chroma", "Cipher", "Clash", "Cohort", "Conspire", "Constellation", "Converge", "Convoke", "Council's Dilemma", "Counter", "Crew", "Cumulative Upkeep", "Cycling", "Dash", "Deathtouch", "Defender", "Delirium", "Delve", "Destroy", "Detain", "Dethrone", "Devoid", "Devour", "Discard", "Domain", "Double Agenda", "Double Strike", "Double", "Dredge", "Echo", "Embalm", "Emerge", "Eminence", "Enchant", "Enrage", "Entwine", "Epic", "Equip", "Escalate", "Eternalize", "Evergreen", "Evoke", "Evolve", "Exalted", "Exchange", "Exert", "Exile", "Exploit", "Explore", "Extort", "Fabricate", "Fading", "Fateful Hour", "Fateseal", "Fear", "Ferocious", "Fight", "First Strike", "Flanking", "Flash", "Flashback", "Flying", "Forecast", "Formidable", "Fortify", "Frenzy", "Fuse", "Goad", "Graft", "Grandeur", "Gravestorm", "Haste", "Haunt", "Hellbent", "Heroic", "Hexproof", "Hidden Agenda", "Hideaway", "Horsemanship", "Imprint", "Improvise", "Indestructible", "Infect", "Ingest", "Inspired", "Intimidate", "Investigate", "Join Forces", "Jump-Start", "Keyword", "Kicker", "Kinship", "Landfall", "Landwalk", "Level Up", "Lieutenant", "Lifelink", "Living Weapon", "Madness", "Manifest", "Mechanics", "Megamorph", "Meld", "Melee", "Menace", "Mentor", "Metalcraft", "Miracle", "Modular", "Monstrosity", "Morbid", "Morph", "Multikicker", "Myriad", "Ninjutsu", "Offering", "Outlast", "Overload", "Parley", "Partner with", "Partner", "Persist", "Phasing", "Planeswalk", "Play", "Poisonous", "Populate", "Proliferate", "Protection", "Provoke", "Prowess", "Prowl", "Radiance", "Raid", "Rally", "Rampage", "Reach", "Rebound", "Recover", "Regenerate", "Reinforce", "Renown", "Replicate", "Retrace", "Reveal", "Revolt", "Riot", "Ripple", "Scavenge", "Scry", "Set in Motion", "Shadow", "Shroud", "Shuffle", "Skulk", "Soulbond", "Soulshift", "Spectacle", "Spell Mastery", "Splice", "Split Second", "Storm", "Strive", "Sunburst", "Support", "Surge", "Surveil", "Suspend", "Sweep", "Tempting Offer", "Threshold", "Totem Armor", "Trample", "Transfigure", "Transform", "Transmute", "Tribute", "Typecycling", "Undaunted", "Undergrowth", "Undying", "Unearth", "Unleash", "Untap", "Vanishing", "Vigilance", "Vote", "Will of the Council", "Wither", "Words"]
 
 def getKeywords(deck):
 
@@ -80,6 +82,9 @@ def getTribalNames(deck):
 
 	return commonTypes
 
+def prettierCardName(cardName):
+	return cardName.replace('The ', '', 1)
+
 def getSignificantCards(deck):
 
 	maxPriceCardPrice = Decimal(0)
@@ -95,16 +100,16 @@ def getSignificantCards(deck):
 
 		price = Decimal(deckCard.jsonData.get("usd", "0.0")) * Decimal(deckCard.count - deckCard.sideboard/3) 
 		if (price > maxPriceCardPrice):
-			maxPriceCard = deckCardName
+			maxPriceCard = prettierCardName(deckCardName)
 			maxPriceCardPrice = price
 
 		cmc = deckCard.jsonData.get("cmc", 0.0) * (deckCard.count - deckCard.sideboard/3)
 		if (cmc > maxCmc):
+			maxCmcCard = prettierCardName(deckCardName)
 			maxCmc = cmc
-			maxCmcCard = deckCardName
 
 		if (deckCard.commander):
-			commanderCard = deckCardName
+			commanderCard = prettierCardName(deckCardName)
 
 	order = {commanderCard: 1, maxPriceCard: 2, maxCmcCard: 3}
 
@@ -114,6 +119,9 @@ def getDeckFormat(deck):
 
 	deckFormats = deckFormat.getDeckFormat(deck)['formats']
 	deckFormats = list(filter(lambda k: deckFormats[k] == True, deckFormats))
+
+	if ('commander' in deckFormats):
+		deckFormats.append('EDH')
 	
 	return sorted(deckFormats, key=lambda k: deckFormat.specificityOfFormat[k], reverse=True)[:1];
 
@@ -123,32 +131,38 @@ def getDeckBudget(deck):
 
 	oneDeckFormat = getDeckFormat(deck)
 
+	if (oneDeckFormat == []):
+		return []
+
 	if (Decimal(deckFormat.budgetPrice[oneDeckFormat[0]]) > price["totalPrice"]):
 		return ['budget']
 	else:
 		return []
 
+def appendNamed(list, names):
+	if (len(names) > 0):
+		random.shuffle(names)
+		list.append(names[0])
+
+
 def nameDeck(deck):
 
 	names = []
 
-	colorNames = colorPairingNames[mtgColors.colorIdentity2String((mtgColors.getDeckColorIdentity(deck)))]
-	tribalNames = getTribalNames(deck)
-	deckFormats = getDeckFormat(deck)
-	keywordsList = getKeywords(deck)
-	cardList = getSignificantCards(deck)
-	deckBudget = getDeckBudget(deck)
+	appendNamed(names, colorPairingNames[mtgColors.colorIdentity2String((mtgColors.getDeckColorIdentity(deck)))])
+	appendNamed(names, getTribalNames(deck))
+	appendNamed(names, getDeckFormat(deck))
+	appendNamed(names, getKeywords(deck))
+	appendNamed(names, getSignificantCards(deck))
+	appendNamed(names, getDeckBudget(deck))
 
-	names.append(colorNames)
-	names.append(tribalNames)
-	names.append(deckFormats)
-	names.append(keywordsList)
-	names.append(cardList)
-	names.append(deckBudget)
+	random.shuffle(names)
+
+	names = names[0:3]
 
 	response = {}
 
-	response['names'] = names
+	response['names'] = " ".join(names)
 
 	return response
 
@@ -157,5 +171,4 @@ def printnDeckNameToConsole(response):
 
 	print ('Deck names:')
 
-	for name in response['names']:
-		print ('*', name)
+	print ('\t"' + response['names'] + '"')
