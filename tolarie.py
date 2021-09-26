293746d6aca8b33bede35806adb783322f094bdc
Petr Prokop <zwei2stein@gmail.com>
2021-09-26 20:39:15 +0200

    Changed to new api for tolarie.cz

--- tolarie.py
+++ tolarie.py
@@ -1,57 +1,50 @@
 import re
-import requests
 
-import util
+import requests
 
 from priceSource import PriceSource
 
+
 class Tolarie(PriceSource):
 
-	def __init__(self, clearCache, cacheTimeout, smartFlush, priority):
-		self.clearCache = clearCache
-		self.cacheTimeout = cacheTimeout
-		self.smartFlush = smartFlush
-		self.sourceName = 'Tolarie'
-		self.supportedCurrency = 'czk'
-		self.cacheDir = '.tolarieCache'
-		self.priority = priority
-		self.baseUrl = "http://tolarie.cz/koupit_karty/"
+    def __init__(self, clearCache, cacheTimeout, smartFlush, priority):
+        self.clearCache = clearCache
+        self.cacheTimeout = cacheTimeout
+        self.smartFlush = smartFlush
+        self.sourceName = 'Tolarie'
+        self.supportedCurrency = 'czk'
+        self.cacheDir = '.tolarieCache'
+        self.priority = priority
+        self.baseUrl = "https://tolarie.cz/koupit_karty/"
 
-	def fetchCardPrice(self, card, page = 0, cheapestPrice = None):
+    def fetchCardPrice(self, card, page=0, cheapestPrice=None):
 
-		response = requests.post(self.baseUrl, params={
-			'name': card.name,
-			'p': page + 1})
+        response = requests.get(self.baseUrl, params={
+            'name': card.name,
+            'stored': 'on', 'o': 'price_order', 'od': 'a'})
 
-		start = response.text.find('<table class="kusovky">')
-		end = response.text.find('</table>', start + 1)
-		
-		regexRow = 'class="product_name">(.+?)</'
+        start = response.text.find('<table class="kusovky">')
+        end = response.text.find('</table>', start + 1)
 
-		names = re.findall(regexRow, response.text[start:end]) 
+        regexRow = 'class="product_name">(.+?)</'
 
-		regexRow = '(?:<td class="td_price">\\s*<span class="product_price">([0-9]+) Kč</span>\\s*</td>)|(?:<td class="td_price">\\s*</td>)'
+        names = re.findall(regexRow, response.text[start:end])
 
-		prices = re.findall(regexRow, response.text[start:end], flags=re.DOTALL) 
+        regexRow = '(?:<td class="td_price">\\s*<span class="product_price">([0-9]+) Kč</span>\\s*</td>)|(?:<td class="td_price">\\s*</td>)'
 
-		for name, price in zip(names, prices):
+        prices = re.findall(regexRow, response.text[start:end], flags=re.DOTALL)
 
-			if (price != ''):
+        for name, price in zip(names, prices):
 
-				name = name.replace('&#39;', '\'')
-				name = name.split('(')[0].strip()
-				name = name.split('#')[0].strip()
+            if price != '':
 
-				price = int(price)
+                name = name.replace('&#39;', '\'')
+                name = name.split('(')[0].strip()
+                name = name.split('#')[0].strip()
 
-				if ((cheapestPrice == None or cheapestPrice > price) and name.lower() == card.name.lower()):
-					cheapestPrice = price
+                price = int(price)
 
-		hasNextPage = 'class="">Další</a>' in response.text
+                if (cheapestPrice is None or cheapestPrice > price) and name.lower() == card.name.lower():
+                    cheapestPrice = price
 
-		if hasNextPage:
-			util.printProgress(page)
-			return self.fetchCardPrice(card, page = page + 1, cheapestPrice = cheapestPrice)
-
-		else:
-			return cheapestPrice
+        return cheapestPrice
